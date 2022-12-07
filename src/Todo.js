c938fa5dd934bae877125c0a267756f4a41f479a
Oh TaeJun <rewrite0w0@gmail.com>
2022-12-07 21:24:07 +0900

    ADD: 개발초기설정

---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import './Todo.css';
+
+export default function Todo() {
+  return (
+    <div className="App">
+      <header></header>
+      <main>main</main>
+      <footer></footer>
+    </div>
+  );
+}
